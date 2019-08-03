d23756b8ed350c1fb9d88675cc621202f92ebe73
Jaebak Kim <jbkim@ucsb.edu>
2019-08-03 03:52:06 -0700

    Made script to run all cards in a directory.

---
+++ python/run_cards.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+import os, sys
+import glob
+import argparse
+
+def parseArguments():
+  parser = argparse.ArgumentParser(description="Runs all datacards in a folder that have .txt extension", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument("-i", "--inputDirectory", default="cards", metavar="INPUT_DIR", help="Directory which holds card files.")
+  parser.add_argument("-o", "--outputFile", default="cards.limits", metavar="LIMIT_FILE", help="Output limit file to be ran by plot_limit")
+  args = parser.parse_args()
+  # Check arguments
+  if not os.path.isdir(args.inputDirectory):
+    print ('[Error] There is no directory called '+args.inputDirectory)
+    sys.exit()
+  if len(glob.glob(args.inputDirectory+'/*.txt')) == 0:
+    print ('[Error] There is no txt files in '+args.inputDirectory)
+    sys.exit()
+  if os.path.exists(args.outputFile):
+    print ('[Error] There is already a file called '+args.outputFile)
+    sys.exit()
+  return args
+
+if __name__ == "__main__":
+  args = parseArguments()
+
+  dataCardPaths = glob.glob(args.inputDirectory+'/*.txt')
+
+  os.system("./compile.py")
+  for datacardPath in dataCardPaths:
+    print ("Processes " + datacardPath)
+    cmd = "./run/hig/scan_point.exe --cards -f "+datacardPath + " >> " + args.outputFile
+    os.system(cmd)
+
+  print('Run ./run/hig/plot_limit.exe -n -f '+args.outputFile)
